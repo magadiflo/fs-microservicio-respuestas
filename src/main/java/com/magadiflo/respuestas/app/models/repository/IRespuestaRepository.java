d43e8b953fea66a1ebc53ae5607e20585a3c5c83
Martín <magadiflo@gmail.com>
2022-11-15 11:46:20 -0500

    Escribiendo consulta JPQL con JOIN para las respuestas del alumno por examen

--- src/main/java/com/magadiflo/respuestas/app/models/repository/IRespuestaRepository.java
+++ src/main/java/com/magadiflo/respuestas/app/models/repository/IRespuestaRepository.java
@@ -1,9 +1,13 @@
 package com.magadiflo.respuestas.app.models.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.magadiflo.respuestas.app.models.entity.Respuesta;
 
 public interface IRespuestaRepository extends CrudRepository<Respuesta, Long> {
+
+	@Query("SELECT r FROM Respuesta AS r JOIN FETCH r.alumno AS a JOIN FETCH r.pregunta AS p JOIN FETCH p.examen AS e WHERE a.id = ?1 AND e.id = ?2")
+	Iterable<Respuesta> findRespuestasByAlumnoByExamen(Long alumnoId, Long examenId);
 
 }
