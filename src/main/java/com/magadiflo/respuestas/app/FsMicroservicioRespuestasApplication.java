e652d3e9ac55ee8729a90c83d9e7a1ca38795f65
Martín <magadiflo@gmail.com>
2022-11-15 11:08:05 -0500

    Configurando Entity Scan
    Con esta configuración le decimos a Spring que nuestras entidades los busque en esos paquetes que le proporcionamos

--- src/main/java/com/magadiflo/respuestas/app/FsMicroservicioRespuestasApplication.java
+++ src/main/java/com/magadiflo/respuestas/app/FsMicroservicioRespuestasApplication.java
@@ -2,9 +2,11 @@ package com.magadiflo.respuestas.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EntityScan(value = { "com.magadiflo.respuestas.app.models.entity", "com.magadiflo.commons.alumnos.models.entity", "com.magadiflo.commons.examenes.models.entity" })
 @SpringBootApplication
 public class FsMicroservicioRespuestasApplication {
 
