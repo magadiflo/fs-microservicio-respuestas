cf0edbc6b5541311909f781a31249338b5fbbc26
Martín <magadiflo@gmail.com>
2022-11-17 23:48:36 -0500

    Añadiendo endPoint en Feign para obtener exámenes ids respondidos

--- src/main/java/com/magadiflo/respuestas/app/clients/IExamenFeignClient.java
+++ src/main/java/com/magadiflo/respuestas/app/clients/IExamenFeignClient.java
@@ -1,8 +1,11 @@
 package com.magadiflo.respuestas.app.clients;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.magadiflo.commons.examenes.models.entity.Examen;
 
@@ -11,5 +14,8 @@ public interface IExamenFeignClient {
 
 	@GetMapping(path = "/{id}")
 	Examen obtenerExamenPorId(@PathVariable Long id);
+
+	@GetMapping(path = "/respondidos-por-preguntas")
+	List<Examen> obtenerExamenesIdsPorPreguntasIdRespondidas(@RequestParam List<Long> preguntaIds);
 
 }
